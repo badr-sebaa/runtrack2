69b4828d205aec3abc0484db282b7b515f4af348
Badr <badr.sebaa@laplateforme.io>
2021-11-02 13:18:45 +0100

    job01

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,4 +1,27 @@
-
 <?php
-echo "Hello LaPlateforme!";
+
+$str = "LaPlateforme";
+echo $str;
+
+$str2 = "vive";
+
+$str3 = "!";
+
+echo "<br/>".$str2.$str. $str3;
+
+$val = 6;
+
+echo "<br/>".$val."<br/>" ;
+
+echo $val + 4;
+
+$myBool = true;
+
+echo "<br/>".$myBool;      
+
+$myBool = false;
+
+echo $myBool; 
+
+
 ?>
