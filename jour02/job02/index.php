5ddc04ddd572847c2c1759e27b85590bf2f4c5db
Badr <badr.sebaa@laplateforme.io>
2021-11-03 11:14:19 +0100

    job 02 and 03

--- jour02/job02/index.php
+++ jour02/job02/index.php
@@ -1,4 +1,11 @@
 <?php
 
+$i=0;
 
+while($i<=1336):
+    $i++;
+    if($i !== 26 and $i != 37 and $i != 88 and $i != 1111 and $i != 3233){
+    echo "<br/>".$i;
+     }
+endwhile;
 ?>
---
+++ jour02/job03/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$i=0;
+
+while($i<=1336):
+    $i++;
+    if($i <= 20){
+    echo "<br/>"."<i>".$i."</i>";
+     }
+    elseif($i >= 25 and $i <=50 and $i != 42){
+        echo "<br/>"."<u>".$i."</u>";
+         }
+    elseif($i == 42){
+        echo "<br/>"."La Plateforme_";
+    }
+    else{echo "<br/>".$i;}
+endwhile;
+?>
