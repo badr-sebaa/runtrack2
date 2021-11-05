7d9ea564da0867061e57ee815f24c21952346177
Badr <badr.sebaa@laplateforme.io>
2021-11-05 10:50:28 +0100

    JOB 07

--- jour03/job07/index.php
+++ jour03/job07/index.php
@@ -1,21 +1,11 @@
 <?php
 
 $str = 'Certaines choses changent, et d autres ne changeront jamais.'; 
+ 
+        $var1 = $str[-1];
+        $str[-1] = $str[0];
+        $str[0] = $var1;
 
-for($i=0;isset($str[$i]);$i++){
-    for($y=1;isset($str[$y]);$y++){
-        if($i == 0){
-        $var1 = $str[$i-1];
-        $str[$i-1] = $str[$i];
-        $str[$i] = $var1;
-        }
-        else{
-        $var1 = $str[$y];
-        $str[$y] = $str[$i];
-        $str[$i] = $var1;
-        }
-        
-    }
-}
 echo $str;
+
 ?>
