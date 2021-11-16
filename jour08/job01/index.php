95d20014258f4465115c7c1afdbf46b50c7079cd
Badr <badr.sebaa@laplateforme.io>
2021-11-16 21:28:49 +0100

    jour 08

--- jour08/job01/index.php
+++ jour08/job01/index.php
@@ -9,7 +9,7 @@
         $bdd  =mysqli_connect("localhost" , "root" ,"root","jour08");
         $req = mysqli_query($bdd , "SELECT * FROM etudiants");
         $res = mysqli_fetch_all($req);
-        
+        var_dump($res);
     ?>
 
         <!-- CREATION DU TABLEAU -->
